1560d08dd992d084b9091f1c1cf5d4589dcb8cbd
mikhail <michael.maidan1998@gmail.com>
2025-04-27 12:56:39 +0200

    faet: implement copy and rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const srcDir = new URL("./files", import.meta.url);
+  const destDir = new URL("./files_copy", import.meta.url);
+
+  try {
+    const stat = await fs.stat(srcDir);
+    if (!stat.isDirectory()) throw new Error();
+
+    await fs.cp(srcDir, destDir, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
