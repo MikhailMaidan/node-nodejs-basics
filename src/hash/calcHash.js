8fc0b4893c73655805361d2f49177ace186838c1
mikhail <michael.maidan1998@gmail.com>
2025-04-27 22:13:25 +0200

    feat: realize the modules and hash two tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { createHash } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = new URL(
+    "./files/fileToCalculateHashFor.txt",
+    import.meta.url
+  );
+  const hash = createHash("sha256");
+
+  try {
+    const stream = fs.createReadStream(filePath);
+    for await (const chunk of stream) {
+      hash.update(chunk);
+    }
+
+    console.log(hash.digest("hex"));
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await calculateHash();
