266cd873df3a3a37e580af702ec176a34bbbf93e
mikhail <michael.maidan1998@gmail.com>
2025-04-28 10:46:06 +0200

    feat: implement "streams" three tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, callback) {
+      try {
+        const str = chunk.toString();
+        const rev = str.split("").reverse().join("");
+        callback(null, rev);
+      } catch (err) {
+        callback(err);
+      }
+    },
+  });
+
+  await pipeline(process.stdin, reverse, process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = new URL("./files/fileToWrite.txt", import.meta.url);
+
+  const outStream = fs.createWriteStream(filePath);
+
+  await new Promise((resolve, reject) => {
+    process.stdin.once("error", () => reject(new Error("FS operation failed")));
+    outStream.once("error", () => reject(new Error("FS operation failed")));
+
+    outStream.once("finish", resolve);
+
+    process.stdin.pipe(outStream);
+  });
 };
 
 await write();
