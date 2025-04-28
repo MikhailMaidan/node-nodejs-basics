84a94e12f0d7a66e372f63a43bf6298acd1d9d13
mikhail <michael.maidan1998@gmail.com>
2025-04-28 19:56:27 +0200

    feat: do the lst two points of Child Process task!

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -7,8 +7,11 @@ const spawnChildProcess = async (args) => {
   );
 
   const child = spawn(process.execPath, [scriptPath, ...args], {
-    stdio: "inherit",
+    stdio: ["pipe", "pipe", "inherit"],
   });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 
   await new Promise((resolve) => child.on("exit", resolve));
 };
--- src/wt/testingWorker.mjs
+++ src/wt/testingWorker.mjs
@@ -16,3 +16,6 @@ worker.once("error", (err) => {
 });
 
 worker.postMessage(10);
+
+// this module was written specifically to test whether the worker.js does its job properly, so I leafe it here for clarification.
+// it's enough to paste node src/wt/testingWorker.mjs in the console to the the calculated result!!!!
